3eeb7f1cd105a213bb07cf9a30457c5ade6493e8
Francis Hilaire <Prometee@users.noreply.github.com>
2024-06-11 16:28:01 +0200

    Fix spaces

--- src/Collector/PluginClientFactory.php
+++ src/Collector/PluginClientFactory.php
@@ -45,10 +45,10 @@ final class PluginClientFactory
     /**
      * @param ClientInterface|HttpAsyncClient $client
      * @param Plugin[]                        $plugins
-     * @param array{client_name?: string} $options
+     * @param array{client_name?: string}     $options
      *
      *        - client_name: to give client a name which may be used when displaying client information like in
-     *         the HTTPlugBundle profiler.
+     *         the HTTPlugBundle profiler
      *
      * @see PluginClient constructor for PluginClient specific $options.
      *
